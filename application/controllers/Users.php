61b1ad130abe11d2b58cafba46926c75c566edb9
Abel <email_id>
2016-05-25 00:11:05 -0400

    login usuarios agregado desde web y android

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
 	####################################################################
-	public function __construct()
-	{
+	public function __construct(){
 		parent::__construct();
       $this->load->library('session');
       $this->load->model('model_template');
--- application/models/Model_posts.php
+++ application/models/Model_posts.php
@@ -14,7 +14,11 @@ class Model_Posts extends Model_Template
       return $this->db->insert_id();
    }
 
-   function get_list_comments(){
+   function get_list_activities($user_id){
+
+      $sql_user = "";
+
+
       $query = $this->db->query("SELECT * FROM
             (
             (SELECT posts.*, 
@@ -23,7 +27,11 @@ class Model_Posts extends Model_Template
          FROM posts 
                JOIN posts AS projects ON projects.id_post=posts.project_id
                JOIN posts AS forums ON forums.id_post=posts.forum_id   
-               WHERE posts.post_type='comment' OR  posts.post_type='forum'      
+               WHERE (posts.post_type='comment' OR  posts.post_type='forum')
+                AND posts.user_id='".$user_id."'
+                AND projects.user_id='".$user_id."'
+                AND forums.user_id='".$user_id."'
+
                ORDER BY posts.post_date DESC, posts.post_register_date DESC)
          UNION
          (
@@ -33,23 +41,26 @@ class Model_Posts extends Model_Template
                                  FROM posts 
       JOIN posts AS projects ON projects.id_post=posts.project_id      
       WHERE posts.post_type='forum'
+        AND posts.user_id='".$user_id."'
+        AND projects.user_id='".$user_id."'                           
+
       ORDER BY posts.post_date DESC, posts.post_register_date DESC)) AS tablex
       ORDER BY tablex.id_post DESC");         
       return $query->result_array();
    }
 
-   function get_list_projects(){
+   function get_list_projects($user_id){
       $query = $this->db->query("SELECT * FROM posts 
-      WHERE post_type='project' 
+      WHERE post_type='project' AND user_id='".$user_id."'
       ORDER BY post_date DESC, post_register_date DESC;");         
       return $query->result_array();
    }
 
-   function get_list_forums(){
+   function get_list_forums($user_id){
       $query = $this->db->query("SELECT forums.id_post AS id_forum, projects.id_post AS id_project, forums.post_title AS forum_title, projects.post_title AS project_title 
       FROM posts forums
       JOIN  posts projects ON forums.parent_id = projects.id_post
-      WHERE forums.post_type='forum'      
+      WHERE forums.post_type='forum'  AND projects.user_id='".$user_id."'
       ORDER BY forums.parent_id");         
       return $query->result_array();
    }
